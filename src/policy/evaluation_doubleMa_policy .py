bab9e2ca19e45368a1a437c830b4cf527b2dea83
Shaoxuan Gao <EUROPRO\shaoxuan.gao@bej-lt-pf28la8v.euro-pro.local>
2021-11-14 16:02:34 +0800

    updat doubleMAStrategy

--- src/policy/evaluation_doubleMa_policy .py
+++ src/policy/evaluation_doubleMa_policy .py
@@ -8,27 +8,43 @@ import pandas as pd
 # plt.rcParams["font.sans-serif"] = ["SimHei"]
 #  plt.rcParams["axes.unicode_minus"] = False
 print(ak.__version__)
-class MA14_Strategy(bt.Strategy):
+class EvaluationDoubleMA_Strategy(bt.Strategy):
     params = (("maperiod", 14), ('printlog', False),)    
-
+    value_buffer = 0;
     def __init__(self):
         self.data_close= self.datas[0].close
         self.order = None
         self.buy_price = None
         self.buy_comm = None
-        self.sma = bt.ind.SimpleMovingAverage(self.datas[0],period = self.params.maperiod)
+        self.value_buffer = self.broker.get_value()
+        self.sma_fast = bt.ind.SimpleMovingAverage(self.datas[0],period = self.params.maperiod)
+        self.sma_slow = bt.ind.SimpleMovingAverage(self.datas[0],period = 12*self.params.maperiod)
 
     def next(self):
-        if self.order:
+        portfolio_value = self.broker.get_value()
+        if(portfolio_value/self.value_buffer < 0.9):
+            self.order = self.order_target_percent(target = 0)
+            self.log("止损")
+            self.value_buffer = portfolio_value
             return
-        if not self.position:
-            if(self.data_close[0] > self.sma[0]):
+        if(portfolio_value/self.value_buffer > 1.3):
+            self.order = self.order_target_percent(target = 0)
+            self.log("止盈")
+            self.value_buffer = portfolio_value
+            return
+        stock_value = self.broker.get_value([self.data])
+        stock_precentage = stock_value/portfolio_value
+        self.log("total Value, %.2f , stock value, %.2f stock precentage, %.3f" % (portfolio_value,stock_value,stock_precentage))
+        target_precent = stock_precentage
+        if(self.sma_fast[0] > self.sma_slow[0] and stock_precentage < 1):
                 self.log("buy create, %.2f"%self.data_close[0])
-                self.order = self.buy()
+                target_precent = 1 if 0.1 + stock_precentage >=1 else 0.1 + stock_precentage 
+        elif (self.sma_fast[0] <= self.sma_slow[0] and stock_precentage > 0):
+                self.log("sell create, %2f" % self.data_close[0])
+                target_precent = 0 if  stock_precentage - 0.2 <= 0 else stock_precentage - 0.2 
         else:
-            if self.data_close[0] < self.sma[0]:
-                self.log("self create, %2f" % self.data_close[0])
-                self.order = self.sell()
+            return
+        self.order = self.order_target_percent(target = target_precent)
     
     def log(self,txt,dt = None,do_print = False):
         if self.params.printlog or do_print:
@@ -71,7 +87,7 @@ class MA14_Strategy(bt.Strategy):
 
 def main(code = "600070", start_cash = 1e5,stake = 100, commission_fee = 0.001):
     cerebro = bt.Cerebro() 
-    cerebro.optstrategy(MA14_Strategy, maperiod=range(10, 20))
+    cerebro.optstrategy(EvaluationDoubleMA_Strategy, maperiod=range(10, 20))
     stock_hfq_df = ak.stock_zh_a_hist(symbol=code,adjust="hfq",start_date='20000101', end_date='20210617').iloc[:,:6]
     stock_hfq_df.columns = [
         'date',
@@ -82,13 +98,13 @@ def main(code = "600070", start_cash = 1e5,stake = 100, commission_fee = 0.001):
         'volume',
     ]
     stock_hfq_df.index = pd.to_datetime(stock_hfq_df['date'])
-    start_date = datetime(2000, 1, 2)
-    end_date = datetime(2021, 6, 16)
+    start_date = datetime(2010, 1, 2)
+    end_date = datetime(2020, 1, 3)
     data = bt.feeds.PandasData(dataname=stock_hfq_df, fromdate=start_date, todate=end_date) 
     cerebro.adddata(data)
     cerebro.broker.setcash(start_cash)
     cerebro.broker.setcommission(commission= commission_fee)
-    cerebro.addsizer(bt.sizers.FixedSize,stake = stake)
+    # cerebro.addsizer(bt.sizers.FixedSize,stake = stake)
     print("期初总资金: %.2f" % cerebro.broker.getvalue())
     cerebro.run(maxcpus=1)  # 用单核 CPU 做优化
     print("期末总资金: %.2f" % cerebro.broker.getvalue())
